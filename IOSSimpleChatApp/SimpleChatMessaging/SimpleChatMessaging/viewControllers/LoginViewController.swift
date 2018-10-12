8a6478bfd17a2131040ae60e1ad5cee2e06d7466
ljbdelacruz <ljbdelacruz123@gmail.com>
2018-10-12 11:43:55 +0800

    updates

---
+++ IOSSimpleChatApp/SimpleChatMessaging/SimpleChatMessaging/viewControllers/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  SimpleChatMessaging
+//
+//  Created by Lainel John Dela Cruz on 12/10/2018.
+//  Copyright © 2018 Lainel John Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var UIEmail: UITextField!
+    @IBOutlet weak var UIPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func OnLoginClick(_ sender: Any) {
+    }
+    
+}
---
+++ IOSSimpleChatApp/SimpleChatMessaging/SimpleChatMessaging/viewControllers/SignupViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignupViewController.swift
+//  SimpleChatMessaging
+//
+//  Created by Lainel John Dela Cruz on 12/10/2018.
+//  Copyright © 2018 Lainel John Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var UIEmail: UITextField!
+    @IBOutlet weak var UIName: UITextField!
+    @IBOutlet weak var UIPassword: UITextField!
+    @IBOutlet weak var UIRPassword: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
